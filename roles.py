bfa7fff7f6751c5daeee712ef6494667e0dc669c
romeo <nyxnyxnyx66@gmail.com>
2025-11-03 00:46:46 +0300

    Добавление множественного присваивания цвета. Добавление установки точного цвета

--- roles.py
+++ roles.py
@@ -127,40 +127,106 @@ class Don(Mafia):
         return random.choice(self.get_players(sheriff=UNKNOWN))
 
 class Knowledge:
-    total_suspection = 9
+    total_suspection = 36
     max_suspection = total_suspection / 3
-    min_suspction = 0
+    base_suspection = total_suspection / 9
+    min_suspection = 0
     def __init__(self, player_id):
         self.knowledge = pd.DataFrame(
             index=list(range(1, 11)),
             data={
-                'color':0, # точное знание о цвете игрока
-                'suspection':1, # коэффициент подозрения
+                'suspection':self.base_suspection, # коэффициент подозрения
                 'sheriff':0, # значение о шерифстве игрока
                 'vendetta':0, # (только для мафии) коэффициент неудобных мирных для мафии
+                'alive':1, # жив ли игрок
+                'lock':0 # зафиксировать цвет игрока
             }
         )
-        self.knowledge = self.knowledge.astype({'color':'Int8', 'suspection':'float32',
-            'sheriff':'Int8', 'vendetta':'float32'})
+        self.knowledge = self.knowledge.astype(
+            {'suspection':'float32', 'sheriff':'Int8', 'vendetta':'float32',
+             'alive':'Int8', 'lock':'Int8'})
         self.knowledge.drop(index=player_id, inplace=True)
+
+    def get_players(self, alive:int=None, lock:int=None, sheriff:int=None,
+            players_id:list[int]=None, as_df=False):
+        result = self.knowledge.copy()
+        if alive is not None:
+            result.query('alive == @alive', inplace=True)
+        if lock is not None:
+            result.query('lock == @lock', inplace=True)
+        if sheriff is not None:
+            result.query('sheriff == @sheriff', inplace=True)
+        if players_id is not None:
+            result.query('index in @players_id', inplace=True)
+        if as_df:
+            return result
+        else:
+            return result.index.to_list()
+
+    @property
+    def rest_suspection(self):
+        cnt_alive = self.get_plaeyrs(alive=1)
+        if cnt_alive >= 7:
+            cnt_black = 3
+        elif cnt_alive >= 5:
+            cnt_black = 2
+        elif cnt_alive >= 3:
+            cnt_black = 1
+        for i in self.get_players(color=-1):
+            cnt_black -= 1
+        return cnt_black * self.max_suspection
     
-    def adjust_suspection(self, player_id, value):
-        control_group = self.knowledge[self.knowledge['color'] == 0]['suspection'].copy()
-        current_suspection = control_group[player_id]
+    def increase_sus(self, player_id, value):
+        control_group = self.knowledge.query('lock == 0 and suspection > 0 and alive = 1')
+        control_group = control_group.loc[control_group.index != player_id]
+        current_suspection = self.knowledge.loc[player_id, 'suspection']
+        if value == 0 or current_suspection == self.max_suspection:
+            return
+        real_value = min(value, self.max_suspection - current_suspection)
+        self.knowledge.loc[player_id, 'suspection'] += real_value
+        shared_value = real_value / control_group.count()
+        while real_value > 0:
+            for i in control_group:
+                if i < shared_value:
+                    extra_value += shared_value - i
+                    i['suspection'] = 0
+    
+    def split_sus(self, control_group, value):
+        for i in control_group:
+
+    def apply_suspection(self, player_id, value):
+        control_group = self.get_players(lock=0, as_df=True)['suspection']
+        control_group = control_group.loc[control_group.index != player_id]
+        current_suspection = self.knowledge.loc[player_id, 'suspection']
+        if value > 0 and current_suspection == self.max_suspection:
+            return
+        if value < 0 and current_suspection == self.min_suspection:
+            return
         if value >= 0:
             real_value = min(value, self.max_suspection - current_suspection)
         else:
-            real_value = max(value, -current_suspection - self.min_suspction)
-        control_group[player_id] += real_value
+            real_value = max(value, -current_suspection - self.min_suspection)
+        self.knowledge.loc[player_id, 'suspection'] += real_value
         adjust_value = real_value / control_group.count()
-        control_group.loc[control_group.index != player_id] -= adjust_value
+        control_group = control_group - adjust_value
+        control_group = control_group.clip(self.min_suspection, self.max_suspection)
         self.knowledge.update(control_group)
 
+    def set_exact_color(self, player_id, color):
+        self.knowledge.loc[player_id, 'lock'] = 1
+        if color == 1:
+            self.apply_suspection(player_id, -self.max_suspection)
+        elif color == -1:
+            self.apply_suspection(player_id, self.max_suspection)
+    
 
-if __name__=='__main__':
+if __name__== '__main__':
     s = Knowledge(1)
-    s.adjust_suspection(2, 3)
-    s.adjust_suspection(5, 3)
-    s.adjust_suspection(4, 3)
+    s.apply_suspection(2, 3)
+    s.set_exact_color(7, 1)
+    s.set_exact_color(8, -1)
+    s.set_exact_color(9, -1)
+    s.set_exact_color(10, -1) # все коэффициенты должны учесть относительность друг друга
     print(s.knowledge)
+    print(s.knowledge['suspection'].sum())
     
---
+++ sus_model.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+
+class Knowledge:
+    total_sus = 36
+    max_sus = total_sus / 3
+    base_sus = total_sus / 9
+    min_sus = 0
+    def __init__(self, player_id):
+        self.table = pd.DataFrame(
+            index=list(range(1, 11)),
+            data={
+                'suspection':self.base_sus, # коэффициент подозрения
+                'sheriff':0, # значение о шерифстве игрока
+                'vendetta':0, # (только для мафии) коэффициент неудобных мирных для мафии
+                'alive':1, # жив ли игрок
+                'lock':0 # зафиксировать цвет игрока
+            }
+        )
+        self.table = self.table.astype(
+            {'suspection':'float32', 'sheriff':'Int8', 'vendetta':'float32',
+             'alive':'Int8', 'lock':'Int8'})
+        self.table.drop(index=player_id, inplace=True)
+
+    def change_sus(self, index, value): 
+        if not isinstance(index, list):
+            index = [index]
+        changed_fact = 0
+        for i in index:
+            cur_sus = self.table.loc[i, 'suspection']
+            if value > 0:
+                real_value = min(value, self.max_sus - cur_sus)
+            else:
+                real_value = max(value, -cur_sus)
+            self.table.loc[i, 'suspection'] += np.float32(real_value)
+            changed_fact += np.float32(real_value)
+        return changed_fact
+
+    def share_sus(self, index, value):
+        if value > 0:
+            target_group = self.table\
+                .query('index in @index and lock == 0 and suspection < @self.max_sus')\
+                .index.to_list()
+        elif value < 0:
+            target_group = self.table\
+                .query('index in @index and lock == 0 and suspection > @self.min_sus')\
+                .index.to_list()
+        n_players = len(target_group)
+        if n_players == 0:
+            return 0
+        shared_sus = value / n_players
+        value -= self.change_sus(target_group, shared_sus)
+        return value
+
+    def adjust_sus(self, index, value):
+        if isinstance(index, int):
+            index = [index]
+        align_group = self.table\
+            .query('lock == 0 and index not in @index').index.to_list()
+        target_group = self.table\
+            .query('lock == 0 and index in @index').index.to_list()
+        if abs(value) > 0:
+            rest_value = -self.change_sus(target_group, value)
+            while abs(rest_value) > 0.0001:
+                rest_value = self.share_sus(align_group, rest_value)
+        else:
+            return
+
+    def set_color(self, index, color):
+        if isinstance(index, int):
+            index = [index]
+        self.adjust_sus(index, 12 * -color)
+        self.table.loc[index, 'lock'] = 1
+
+s = Knowledge(1)
+s.set_color(2, 1)
+s.set_color(3, 1)
+s.set_color(4, 1)
+s.set_color(8, -1)
+
+print(s.table)
